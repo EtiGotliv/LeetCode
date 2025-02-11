aa0470d9466439e14e33ae589d137833191618c9
Eti Gotliv <etigotliv2134@gmail.com>
2025-02-11 21:01:38 +0200

    Remove Occurrences

---
+++ Remove All Occurrences of a Substring/removeOccurrences.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder str = new StringBuilder();
+        for(int i = 0 ; i < s.length(); i++){
+           str.append(s.charAt(i));
+           if(str.length()>=part.length() && str.substring(str.length()-part.length()).equals(part))
+             str.setLength(str.length()-part.length());
+
+        }
+        return str.toString();
+    }
+}
